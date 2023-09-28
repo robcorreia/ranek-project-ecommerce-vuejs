bbb5329810c2d037fb3739610c774a45a7482c49
Robson <robccorreia@gmail.com>
2023-09-28 16:16:24 -0300

    feat: add Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <ProductsSearch />
+    <ProductsList />
+  </section>
 </template>
 
 <script>
+import ProductsSearch from "@/components/ProductsSearch.vue";
+import ProductsList from "@/components/ProductsList.vue";
+
 export default {
   name: "Home",
+  components: {
+    ProductsSearch,
+    ProductsList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
