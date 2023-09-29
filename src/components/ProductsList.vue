2b634d61806d14ae3360bd34c5b90a686b9acf3b
Robson <robccorreia@gmail.com>
2023-09-29 15:30:48 -0300

    feat: add axios instance

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,7 +1,37 @@
-<template></template>
+<template>
+  <section class="container-product">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.photos"
+        :src="product.photos[0].src"
+        :alt="product.photos[0].title"
+      />
+      <h2 class="title">{{ product.name }}</h2>
+      <p class="price">{{ product.price }}</p>
+      <p>{{ product.description }}</p>
+    </div>
+  </section>
+</template>
 
 <script>
-export default {};
+import { api } from "@/service/index";
+
+export default {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  methods: {
+    async getProducts() {
+      const response = await api.get("/product");
+      this.products = response.data;
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
 </script>
 
 <style></style>
