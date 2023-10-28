b94450144166e526bb8bcb2f4b1c9e288e119616
Robson <robccorreia@gmail.com>
2023-10-28 10:49:58 -0300

    feat: add User Update

--- src/views/User/UserEdit.vue
+++ src/views/User/UserEdit.vue
@@ -1,10 +1,32 @@
 <template>
-  <p>editar usuario</p>
+  <section>
+    <UserForm>
+      <button class="btn" @click.prevent="userUpdate">Atualizar Usuário</button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
+import UserForm from "@/components/UserForm.vue";
+import { api } from "@/service";
+
 export default {
   name: "UserEdit",
+  components: { UserForm },
+  methods: {
+    async userUpdate() {
+      try {
+        await api.put(
+          `/user/${this.$store.state.user.id}`,
+          this.$store.state.user
+        );
+        this.$store.dispatch("getUser", this.$store.state.user.id);
+        this.$router.push({ name: "user" });
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+  },
 };
 </script>
 
