9c77080a64a8533c4a4e82703ec2d295ed0db69a
Robson <robccorreia@gmail.com>
2023-10-29 19:12:49 -0300

    feat: add Orders and Sals

--- src/views/User/UserOrders.vue
+++ src/views/User/UserOrders.vue
@@ -1,11 +1,67 @@
 <template>
-  <p>lista de compras</p>
+  <section>
+    <div v-if="orders">
+      <h2>Compras</h2>
+      <div
+        class="products-wrapper"
+        v-for="(order, index) in orders"
+        :key="index"
+      >
+        <ProductItem v-if="order.product" :product="order.product">
+          <p class="saler"><span>Vendedor: </span>{{ order.saler_id }}</p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItem from "@/components/ProductItem.vue";
+import { api } from "@/service";
+import { mapState } from "vuex";
+
 export default {
   name: "UserOrders",
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      orders: null,
+    };
+  },
+  computed: {
+    ...mapState(["user", "login"]),
+  },
+  methods: {
+    async getSales() {
+      const response = await api.get(`/transaction?buyer_id=${this.user.id}`);
+      this.orders = response.data;
+    },
+  },
+  watch: {
+    login() {
+      this.getSales();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getSales();
+    }
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.product-wrapper {
+  margin-bottom: 40px;
+}
+
+.saler span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
--- src/views/User/UserSales.vue
+++ src/views/User/UserSales.vue
@@ -1,11 +1,86 @@
 <template>
-  <p>lista de vendas</p>
+  <section>
+    <div v-if="sales">
+      <h2>Vendas</h2>
+      <div class="products-wrapper" v-for="(sale, index) in sales" :key="index">
+        <ProductItem v-if="sale.product" :product="sale.product">
+          <p class="saler">
+            <span>Comprador:</span>
+            {{ sale.buyer_id }}
+          </p>
+        </ProductItem>
+        <div class="delivery">
+          <h3>Entrega:</h3>
+          <ul v-if="sale.address">
+            <li v-for="(value, key) in sale.address" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItem from "@/components/ProductItem.vue";
+import { api } from "@/service";
+import { mapState } from "vuex";
+
 export default {
-  name: "UserSales",
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      sales: null,
+    };
+  },
+  computed: {
+    ...mapState(["user", "login"]),
+  },
+  methods: {
+    async getSales() {
+      const response = await api.get(`/transaction?saler_id=${this.user.id}`);
+      console.log(response);
+      this.sales = response.data;
+    },
+  },
+  watch: {
+    login() {
+      this.getSales();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getSales();
+    }
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.products-wrapper {
+  margin-bottom: 40px;
+}
+
+.saler span {
+  color: #e80;
+}
+
+.delivery {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+h3 {
+  margin: 0px;
+  justify-self: end;
+}
+</style>
